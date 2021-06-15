263c1e8d0045b1980a5b6e46c7eb712b0f16099b
Sever_IN <71351429+Sever-IN@users.noreply.github.com>
2021-06-15 20:26:46 +0300

    Add files via upload

---
+++ launcher.py
@@ -0,0 +1,12 @@
+from lib.bot import MyBot
+
+# class Launcher:
+#     def __init__(self, token: str):
+#         self.session = MyBot(token)
+#         self.session.run()
+
+if __name__ == '__main__':
+    tokens = open('./data/tokens/discord.0', 'r', encoding='utf-8')
+    token = tokens.read().split('\n')[0]
+    session = MyBot(token)
+    session.run()
